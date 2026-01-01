6f94dab5ab785a39eab8a3c8ceff5c764ab2ecfe
hafidjs <afidzpratama@gmail.com>
2026-01-01 13:11:30 +0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:hf_shop/features/shop/controllers/home/home_controller.dart';
+import 'package:hf_shop/firebase_options.dart';
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hf_shop/my_app.dart';
 
-void main() {
+Future<void> main() async {
   // widget flutter binding
   // final widgetBinding = WidgetsFlutterBinding.ensureInitialized();
 
@@ -13,6 +15,10 @@ void main() {
   // FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
 
   // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   Get.put(HomeController());
   runApp(const MyApp());
 
